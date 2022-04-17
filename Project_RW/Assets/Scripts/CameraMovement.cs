80b859ca87490910a33e4fa4579c13911346559a
WMoth <dldngud97@naver.com>
2022-04-17 15:07:06 +0900

    character movement

---
+++ Project_RW/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+    [SerializeField]
+    private float speed;
+
+    private Vector3 offset;
+    private Vector3 targetPos;
+
+    private void Start()
+    {
+        if (target == null) return;
+        offset = transform.position - target.position;
+    }
+
+    private void Update()
+    {
+        if (target == null) return;
+        targetPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetPos, speed * Time.deltaTime);
+    }
+}
